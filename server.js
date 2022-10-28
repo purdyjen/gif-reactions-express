e11a36a8fe16d25d9e1de87f3a5c70ac3ccb11ca
purdyjen <purdyjen@me.com>
2022-10-28 17:03:52 -0400

    initial files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.use(express.static(path.join(__dirname, "app/public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "app/public/index.html"));
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
